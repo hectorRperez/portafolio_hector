b3802595acbfa836614d9a2d99d7750845865e51
Hector Perez <hector.perez@webleadsgroup.com>
2022-06-05 17:36:30 -0400

    update styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,6 @@ navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 // ====== SCROLL REVEAL ANIMATION ===========
 const sr = ScrollReveal({
-	origin: 'top',
 	distance: '200px',
 	duration: 2000,
 	reset: true,
